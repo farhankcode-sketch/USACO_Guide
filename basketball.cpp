c8ef1774ce5b04caf4dbc5e3d530f70b59f1a7a9
Farhan Kabir <farhank.software@example.com>
2026-08-08 09:35:37 +0530

    MY first problem on basketball

---
+++ basketball.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main(){
+    string basketball;
+    cin>> basketball;
+    int k;
+    k = basketball.length()
+    int A = 0;
+    int B = 0;
+    for (int i =0, i < k , i+=2){
+        if (basketball[i] == 'A'){
+            if (basketball[i+1] == '1'){
+                A = A +1 ;
+            }else {
+                A = A +2;
+            }
+        }
+        else {
+            if (basketball[i+1] == '1'){
+                B = B +1 ;
+            }else {
+                B = B +2;
+        }
+    }
+    #WE KNOW THE VALUE OF A AND B NOW JUST ANNOUNCE THE WINNER BY COMPARING WHO IS WINNNG !
+    if (A > B){
+        cout << A;
+    } else { 
+        cout << B;
+    }
+    return 0;
+}
